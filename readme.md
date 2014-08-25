43beb9f4747753d204ea90b527bf418ab712d01e
cloudcome <cloudcome@163.com>
2014-08-25 17:02:01 +0800

    0825

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ STATIC SERVER => sts
 
 # INSTALL
 ```
-npm install sts -g
+npm i sts -g
 ```
 
 # USAGE
@@ -12,6 +12,16 @@ npm install sts -g
 cd your directory
 sts 18080 your server name
 ```
-* A HTTP static server will run in this directory.
-* `18080` **required**
-* `your server name` **optional**
+
+* HTTP静态服务器将运行在当前目录。
+* 端口，必须: `18080`
+* 名称（防止在cmd窗口运行多个静态服务器，不知道哪个是哪个），可选: `your server name`
+
+
+# VERSION
+## v0.0.12
+* 增加了对markdown文件的支持，支持`.md`和`.markdown`两种文件后缀
+
+## v0.0.1
+* 支持了全局安装以及路径适配
+* [链接](http://123.com)
--- sts.js
+++ sts.js
@@ -5,7 +5,9 @@ var http = require('http');
 var path = require('path');
 var fs = require('fs');
 var lib = require('./lib.js');
-
+var markdown = require('markdown-parse');
+var template = fs.readFileSync('./markdown-template.html', 'utf8');
+var regTemplate = /{{markdown}}/;
 
 
 ///////////////////////////////////////////////////////////////////
@@ -38,9 +40,6 @@ if (!/^\d+$/.test(PORT)) {
 var NAME = args.join(' ') || path.basename(WEBROOT);
 
 
-
-
-
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
 //////////////////////////【实现】/////////////////////////////////
@@ -48,7 +47,7 @@ var NAME = args.join(' ') || path.basename(WEBROOT);
 ///////////////////////////////////////////////////////////////////
 var reg = /^\./;
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
     var url = request.url;
     var parse = url2.parse(url);
     var pathname = parse.pathname;
@@ -59,19 +58,27 @@ http.createServer(function(request, response) {
     var relative = path.relative(WEBROOT, filepath);
 
     // 开头打点了，说明是想访问父级目录，绝对禁止
-    if (reg.test(relative)) return lib['500'](response, '非法操作');
+    if (reg.test(relative)) {
+        return lib['500'](response, '非法操作');
+    }
 
-    fs.lstat(filepath, function(err, stats) {
-        if (err) return lib['404'](response);
+    fs.lstat(filepath, function (err, stats) {
+        if (err) {
+            return lib['404'](response);
+        }
 
         if (stats.isDirectory() || stats.isSymbolicLink()) {
 
-            if (lastChar !== '/') return lib['302'](response, pathname + '/' + search);
+            if (lastChar !== '/') {
+                return lib['302'](response, pathname + '/' + search);
+            }
 
             filepath = path.join(filepath, DEFAULTFILE);
 
-            fs.exists(filepath, function(b) {
-                if (!b) return lib['404'](response);
+            fs.exists(filepath, function (b) {
+                if (!b) {
+                    return lib['404'](response);
+                }
 
                 response.writeHead(200, {
                     'content-type': lib.getMime('.html')
@@ -82,16 +89,27 @@ http.createServer(function(request, response) {
             response.writeHead(200, {
                 'content-type': lib.getMime(extname)
             });
-            fs.createReadStream(filepath).pipe(response);
+            if (['.md', '.markdown'].indexOf(extname) > -1) {
+                var text = fs.readFileSync(filepath, 'utf8');
+                var html = markdown(text, function (err, ret) {
+                    if (err) {
+                        return lib['500'](response, err.message);
+                    }
+
+                    response.end(template.replace(regTemplate, ret.html));
+                });
+            } else {
+                fs.createReadStream(filepath).pipe(response);
+            }
         } else {
             lib['500'](response);
         }
     });
-}).listen(PORT, function() {
+}).listen(PORT, function () {
     console.log('############################################################');
     console.log(NAME + ' URL: http://localhost:' + PORT);
     console.log('############################################################');
-}).on('error', function(e) {
+}).on('error', function (e) {
     console.log('############################################################');
     console.log(NAME + ' ERROR:');
     console.log(e.stack);
